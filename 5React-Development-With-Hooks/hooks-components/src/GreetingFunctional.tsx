93fa8639f8da1845235c6256fc71adea24526d03
GigaVardia <gigavardia@gmail.com>
2021-03-06 19:16:42 +0400

    finished chap 5

---
+++ 5React-Development-With-Hooks/hooks-components/src/GreetingFunctional.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface GreetingProps {
+    enteredName: string,
+    message: string,
+    greetingDispatcher: React.Dispatch<{type: string, payload: string}>
+}
+
+export default function Greeting(props: GreetingProps) {
+    console.log("Rendering Greeting");
+
+    const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        props.greetingDispatcher({type: "enteredName", payload: e.target.value})
+        props.greetingDispatcher({type: "message", payload: e.target.value})
+    }
+
+    return (
+        <div>
+            <input type="text" value={props.enteredName} onChange={onChangeName}/>
+            <div>
+                {props.message}
+            </div>
+        </div>
+    )
+}
